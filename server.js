7e6dcbe3e4bf443e4031b1938289e89cf13b8fc0
serhioses <serega.mirosh@gmail.com>
2017-01-17 00:09:00 +0200

    Express server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('app'));
+
+app.listen(port, function () {
+    console.log('Express server is up on ' + port);
+});
