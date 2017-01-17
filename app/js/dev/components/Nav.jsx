a834fedcb5f027c727c015c9e11e851e9a84c0d8
serhioses <serega.mirosh@gmail.com>
2017-01-17 22:55:14 +0200

    style nav bar

--- app/js/dev/components/Nav.jsx
+++ app/js/dev/components/Nav.jsx
@@ -4,14 +4,41 @@ import {Link, IndexLink} from 'react-router';
 export default class Nav extends React.Component {
     constructor(props) {
         super(props);
+        this.onSearch = this.onSearch.bind(this);
+    }
+    onSearch(e) {
+        e.preventDefault();
+        console.log('Not yet wired up!');
     }
     render() {
         return (
-            <div>
-                <h2>Nav component</h2>
-                <IndexLink to="/" className="link" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-                <Link to="/about" className="link" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-                <Link to="/examples" className="link" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li>
+                            <IndexLink to="/" className="link" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" className="link" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" className="link" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search weather"/>
+                            </li>
+                            <li>
+                                <input className="button" type="submit" value="Get Weather"/>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
             </div>
         );
     }
